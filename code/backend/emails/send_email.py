83df63424600baebeb35389712edd7a2dd6eca52
Pengyuan Yu <pengyuany@student.unimelb.edu.au>
2024-10-04 03:38:31 +1000

    Use Enum to ensure the parameter format is correct

--- code/backend/emails/send_email.py
+++ code/backend/emails/send_email.py
@@ -4,16 +4,27 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 from email.header import Header
+from enum import Enum
+
 from config.settings import *
 from text_file import text_file
 import os
 
 
+class FileType(Enum):
+    NONE = 1
+    PATH = 2
+    TXT = 3
+
+
 # file_type {'txt', 'file_path', 'none'}
-# txt means encode the file_content into a txt file and send to the receiver
-# file_path means the file_content is the path of the attached file
-# none means there is no attachment in this email
-def send_email(receiver, subject, content, file_content, file_type='txt'):
+# TXT means encode the file_content into a txt file and send to the receiver
+# PATH means the file_content is the path of the attached file
+# NONE means no file attached
+def send_email(receiver, subject, content, file_content, file_type=FileType.TXT):
+    if not isinstance(file_type, FileType):
+        file_type = FileType.NONE
+        print("file_type format error: file_type has been set to NONE")
     msg = MIMEMultipart()
     msg['From'] = Header(SMTP_USER, 'utf-8')
     msg['To'] = Header(receiver, 'utf-8')
@@ -21,23 +32,22 @@ def send_email(receiver, subject, content, file_content, file_type='txt'):
     body = content
     msg.attach(MIMEText(body + '\n', 'plain', 'utf-8'))
     file = None
-    if file_type == 'txt':
+    if file_type == FileType.TXT:
         file = text_file(file_content)
-    elif file_type == 'file_path':
+    elif file_type == FileType.PATH:
         file = file_content
-    if file_type != 'none':
-        if os.path.exists(file):
-            with open(file, 'rb') as attachment:
-                part = MIMEBase('application', 'octet-stream')
-                part.set_payload(attachment.read())
-                encoders.encode_base64(part)
-                part.add_header(
-                    'Content-Disposition',
-                    f'attachment; filename= {os.path.basename(file)}',
-                )
-                msg.attach(part)
-        else:
-            print(f"File {file} doesn't exist")
+    if file is not None and os.path.exists(file):
+        with open(file, 'rb') as attachment:
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(attachment.read())
+            encoders.encode_base64(part)
+            part.add_header(
+                'Content-Disposition',
+                f'attachment; filename= {os.path.basename(file)}',
+            )
+            msg.attach(part)
+    elif file is not None:
+        print(f"File {file} doesn't exist")
 
     server = None
     try:
@@ -46,7 +56,7 @@ def send_email(receiver, subject, content, file_content, file_type='txt'):
         server.login(SMTP_USER, SMTP_PASSWORD)
         server.sendmail(SMTP_USER, [receiver], msg.as_string())
         print("succeed")
-        if file_type != 'none' and os.path.exists(file):
+        if file is not None and os.path.exists(file):
             try:
                 os.remove(file)
             except Exception as e:
@@ -59,4 +69,4 @@ def send_email(receiver, subject, content, file_content, file_type='txt'):
 
 if __name__ == "__main__":
     send_email('yupengyuanchina@gmail.com', 'test', 'test with file',
-               'This is a test for txt', 'txt')
+               'This is a test for txt', FileType.TXT)
