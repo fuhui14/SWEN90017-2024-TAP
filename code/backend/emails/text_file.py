950f9e9166d46f3f1756cffb832a35989c8f5e56
Pengyuan Yu <pengyuany@student.unimelb.edu.au>
2024-10-04 02:03:16 +1000

    Little change on the file name format when sending emails

--- code/backend/emails/text_file.py
+++ code/backend/emails/text_file.py
@@ -2,7 +2,7 @@ import time
 
 
 def text_file(content):
-    txt_filename = 'result_' + format(time.time(), '.0f')
+    txt_filename = 'result_' + format(time.time(), '.0f') + '.txt'
 
     with open(txt_filename, 'w', encoding='utf-8') as txt_file:
         txt_file.write(content)
