aecfc3c5565c10437feec7824f809ea1080899ec
Pengyuan Yu <pengyuany@student.unimelb.edu.au>
2024-10-15 16:07:52 +1100

    download file api updated to download/

--- code/backend/emails/urls.py
+++ code/backend/emails/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.download_file, name='download_file')
+    path('download/', views.download_file, name='download_file')
 ]
