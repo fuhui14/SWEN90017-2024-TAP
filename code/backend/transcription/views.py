2a76a62a9c28a00bae015b2d496e9ccd5842f14d
ZxZhang01 <zixuzhang2@student.unimelb.edu.au>
2024-10-17 18:38:14 +1100

    Upate comments of views.py

--- code/backend/transcription/views.py
+++ code/backend/transcription/views.py
@@ -11,9 +11,9 @@ import whisper
 
 from speaker_identify.assign_speaker_service import assign_speakers_to_transcription
 from .forms import UploadFileForm
-from .models import File, Transcription  # 确保 File 和 Transcription 模型已定义
+from .models import File, Transcription
 
-# 在应用启动时加载 Whisper 模型
+# load whisper model when the server starts
 model = whisper.load_model("base")
 
 @csrf_exempt
@@ -24,18 +24,21 @@ def transcribe(request):
         if form.is_valid():
             print("Form is valid")
 
-            # 提取用户的邮箱
+            # get user email
             email = form.cleaned_data.get('email')
             print(f"User email: {email}")
 
-            # 保存上传的文件
+            # save the uploaded file 
             file = request.FILES['file']
-            upload_id = uuid.uuid4()  # 生成唯一标识符
+
+            # generate a unique ID for the upload
+            upload_id = uuid.uuid4() 
             original_filename = file.name
             print(f"Original filename: {original_filename}, Upload ID: {upload_id}")
 
-            # 根据结构化存储策略定义文件路径
-            sanitized_email = email.replace('@', '_at_').replace('.', '_dot_')  # 确保路径不包含非法字符
+            # generate the storage path for the file
+            # use at and dot instead of @ and . in the email address
+            sanitized_email = email.replace('@', '_at_').replace('.', '_dot_')
             storage_dir = os.path.join(settings.MEDIA_ROOT, 'uploads', sanitized_email, upload_id.hex)
             try:
                 os.makedirs(storage_dir, exist_ok=True)
@@ -43,6 +46,7 @@ def transcribe(request):
                 print(f"Error creating directory: {e}")
                 return JsonResponse({'error': f'Unable to create storage directory: {str(e)}'}, status=500)
 
+            # get the full path to save the file
             file_path = os.path.join(storage_dir, original_filename)
             print(f"Storage directory: {storage_dir}, File path: {file_path}")
 
@@ -55,12 +59,12 @@ def transcribe(request):
                 print(f"Error saving file: {e}")
                 return JsonResponse({'error': f'Error saving file: {str(e)}'}, status=500)
 
-            # 检查文件是否成功保存
+            # check if the file exists
             if not os.path.exists(file_path):
                 print(f"File does not exist after saving: {file_path}")
                 return JsonResponse({'error': f'File not found after saving: {file_path}'}, status=500)
 
-            # 将文件元数据存储到数据库
+            # save the file metadata to the database
             try:
                 db_file = File.objects.create(
                     email=email,
@@ -75,12 +79,15 @@ def transcribe(request):
                 print("Error saving file metadata to the database:", e)
                 return JsonResponse({'error': f'Database error: Unable to save file metadata: {str(e)}'}, status=500)
 
-            # 转录音频文件并存储转录文本
+            # transcribe the audio file
             try:
                 print(f"Starting transcription for file: {file_path}")
                 if not os.path.exists(file_path):
                     raise FileNotFoundError(f"The file at {file_path} does not exist")
-                transcription = transcribe_audio(file_path)  # 根据操作系统调整路径格式
+                # transcribe the audio file
+                transcription = transcribe_audio(file_path) 
+
+                # speaker identification
                 transcription_with_speaker = assign_speakers_to_transcription(transcription, file_path)
                 Transcription.objects.create(
                     file=db_file,
@@ -94,7 +101,7 @@ def transcribe(request):
                 print(f"Error during transcription or saving transcription for file {file_path}: {e}")
                 return JsonResponse({'error': f'Transcription error: {str(e)}'}, status=500)
 
-            # 返回转录结果
+            # return the transcription result
             print("Returning transcription result")
             return JsonResponse({"transcription": transcription_with_speaker}, safe=False)
 
@@ -110,17 +117,13 @@ def transcribe(request):
     return render(request, 'index.html', {'form': form})
 
 def transcribe_audio(audio_path):
-    """
-    使用 Whisper 模型转录给定的音频文件。
-    """
-    print('Transcribing audio file at path: ' + audio_path)  # 更详细的调试信息
+    print('Transcribing audio file at path: ' + audio_path)
     try:
         system_platform = platform.system()
+        # if the system is Windows, convert the path to a raw string
         if system_platform == 'Windows':
-            # 在 Windows 上，将路径转换为原始字符串格式
             audio_path = r'{}'.format(audio_path)
             print('Path:::' + audio_path)
-        # 在非 Windows 系统上，保持原始的路径格式
 
         result = model.transcribe(audio_path)
         return result
